c4891a8a64e18c8a3e884ab8ea6c34af8d5006b5
DVFDVF <2961329526@qq.com>
2024-04-04 01:02:55 +0800

    修改bug

--- app.js
+++ app.js
@@ -44,7 +44,41 @@ window.addEventListener("appinstalled", (e) => {
 let rbLayer;
 async function installApp() {
   if (deferredPrompt) {
-    deferredPrompt.prompt();
+    deferredPrompt
+      .prompt()
+      .then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          objectElement.remove();
+          showResult("😀 User accepted the install prompt.", true);
+          const loadingContainer = document.getElementById("loadingContainer");
+          const loadingText = document.getElementById("loadingText");
+          loadingContainer.style.display = "flex";
+          let number = 0;
+          let timer = setInterval(function () {
+            if (number < 90) {
+              number += 1;
+              loadingText.innerHTML = `${number}%`;
+            } else if (number < 100) {
+              if (appinstalled) {
+                number += 1;
+                loadingText.innerHTML = `${number}%`;
+              }
+            } else {
+              clearInterval(timer);
+              loadingText.innerHTML = `${number}%`;
+              loadingContainer.style.display = "none";
+              const tipMain = document.getElementById("tipMain");
+              tipMain.style.display = "flex";
+            }
+          }, 80);
+        } else if (choiceResult.outcome === "dismissed") {
+          showResult("😟 User dismissed the install prompt");
+        }
+      })
+      .catch((error) => {
+        console.error("Error occurred while prompting:", error);
+        // 处理出现的错误
+      });
     showResult("🆗 Installation Dialog opened");
   } else {
     rbLayer = document.getElementById("rb-layer");
@@ -81,7 +115,6 @@ async function installApp() {
               }
             }, 80);
           } else if (choiceResult.outcome === "dismissed") {
-            objectElement.remove();
             showResult("😟 User dismissed the install prompt");
           }
         })
