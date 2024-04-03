3e5f397ff2a866ffb1f5fb54316fbfa612721ab4
DVFDVF <2961329526@qq.com>
2024-04-04 00:51:42 +0800

    修改bug

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ window.addEventListener("appinstalled", (e) => {
 });
 let rbLayer;
 async function installApp() {
-  if (deferredPrompt?.choiceResult) {
+  if (deferredPrompt) {
     deferredPrompt.prompt();
     showResult("🆗 Installation Dialog opened");
   } else {
